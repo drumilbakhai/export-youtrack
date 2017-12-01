5f7289e71387bbd2799bc7e4131af53700f78ffb
Drumil Bakhai <drums.b123@gmail.com>
2017-11-30 19:12:17 -0500

    Changed the export-issue to get the IDS for the issues. Also got the comments for a single issue

--- export-issue.py
+++ export-issue.py
@@ -1,6 +1,35 @@
 import requests
+from xml.etree import ElementTree as etree
+
+def getIds(token,project,max_issues,):
+    all_ids = []
+    API = "https://nyuwp.myjetbrains.com/youtrack/rest/issue/byproject/"+project+"?max="+str(max_issues)
+    print(API)
+    headers = {'Authorization': token}
+    r = requests.get(API, headers = headers)
+    tree = etree.fromstring(r.content)
+
+    for child in tree:
+        # print(child.tag, child.attrib['id'])
+        all_ids.append(child.attrib['id'])
+
+    return all_ids
+
+def getCommentsForAnIssue(token, issue_id):
+    API = "https://nyuwp.myjetbrains.com/youtrack/rest/issue/"+issue_id+"/comment"
+    headers = {'Authorization': token}
+    r = requests.get(API,headers = headers)
+    tree = etree.fromstring(r.content)
+    comment_arr = []
+    for child in tree:
+        print(child.attrib['text'])
+        comment_arr.append(child.attrib['text'])
+
+
 file_obj = open('my_token', 'r')
 token = file_obj.read()
-headers = {'Authorization': token}
-r = requests.get('https://nyuwp.myjetbrains.com/youtrack/rest/issue/WP-851/attachment', headers = headers)
-print(r)
+
+#print(getIds(token,'AUTOMATION',700,))
+print(getCommentsForAnIssue(token,'WP-553'))
+
+
