7836a88645a49546bebea554db00fdef087856d3
Drumil Bakhai <drums.b123@gmail.com>
2017-12-01 00:03:12 -0500

    Edited the main code for successfully writing the comments related to each issue under a project file name. Documented the code for better understanding. Also changed the .gitignore file for ignoring .csv files

--- export-issue.py
+++ export-issue.py
@@ -1,35 +1,85 @@
 import requests
+import csv
 from xml.etree import ElementTree as etree
 
-def getIds(token,project,max_issues,):
-    all_ids = []
-    API = "https://nyuwp.myjetbrains.com/youtrack/rest/issue/byproject/"+project+"?max="+str(max_issues)
-    print(API)
-    headers = {'Authorization': token}
-    r = requests.get(API, headers = headers)
-    tree = etree.fromstring(r.content)
+def writeToCSVFile(filename, comments):
+    #print(comments)
 
-    for child in tree:
-        # print(child.tag, child.attrib['id'])
+    # Simply Appending the Issue Id and the comments in a csv file
+    """
+    A simple method to write data to the CSV file
+    :param filename: The name of the file where the data will be stored or written
+    :param comments: The list of comments pertaining to a single issue
+    """
+    with open(filename, 'a') as csvfile:
+        writer = csv.writer(csvfile, dialect='excel')
+        writer.writerow(comments)
+
+
+def getIds(token,project,max_issues):
+    """
+    Method that retrieves the list of all the Issues along with Issue ID in the project
+    :param token: Token for Authorization. Token is read from a file
+    :param project: The name of the project for which the issue ids will be fetched.
+    :param max_issues: An upper limit to retrieve the number of issues.
+    :return:
+    """
+    all_ids = []
+    # Preparing URL for getting list of ids for a given project
+    API = "https://nyuwp.myjetbrains.com/youtrack/rest/issue/byproject/"+project+"?max="+str(max_issues)
+    headers = {'Authorization': token}
+    print(API)
+
+    # Sending request
+    r = requests.get(API, headers = headers)
+    data = etree.fromstring(r.content)
+
+    for child in data:
         all_ids.append(child.attrib['id'])
 
     return all_ids
 
 def getCommentsForAnIssue(token, issue_id):
+    """
+    A sample method to retrieve the list of comments based on the issue id
+    :param token: Token for Authorization. Token is read from a file
+    :param issue_id: Issue id for which the comments will be retrieved
+    :return: Return the list of comments made on a issue
+    """
+    comment_arr = []
+    comment_arr.insert(0, issue_id)
+
+    # Preparing URL for getting all the comments for a given issue
     API = "https://nyuwp.myjetbrains.com/youtrack/rest/issue/"+issue_id+"/comment"
     headers = {'Authorization': token}
+
+    # Sending Request
     r = requests.get(API,headers = headers)
+
+    # Getting the data back in XML Format
     tree = etree.fromstring(r.content)
-    comment_arr = []
+
     for child in tree:
-        print(child.attrib['text'])
-        comment_arr.append(child.attrib['text'])
+        # Formating the output to display the name of user who commented.
+        line = child.attrib['author'] +' commented ==> '+ child.attrib['text']
+        comment_arr.append(line)
 
+    return comment_arr
 
+# Reading Authorization Token
 file_obj = open('my_token', 'r')
 token = file_obj.read()
 
-#print(getIds(token,'AUTOMATION',700,))
-print(getCommentsForAnIssue(token,'WP-553'))
+# Input the project
+wp_project = input()
+filename = wp_project + '.csv'
+max_issues = 700
 
+# Get the list of all Issue id for a project
+list_ids = getIds(token,wp_project,max_issues)
+
+# Iterate over each issue id, retrieve the comments for that issue and append the comments to csv file
+for each_id in list_ids:
+    comments_list = getCommentsForAnIssue(token,each_id)
+    writeToCSVFile(filename,comments_list)
 
